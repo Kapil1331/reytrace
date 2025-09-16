d18e91a7cea24c4925715e1f87c7ce982d17c34e
Kapil1331 <kapiltangsali@gmail.com>
2025-09-17 00:54:51 +0530

    refactor: moving towards modularity

--- main.c
+++ main.c
@@ -2,13 +2,17 @@
 #include <SDL2/SDL_events.h>
 #include <SDL2/SDL_surface.h>
 #include <SDL2/SDL_video.h>
-#include <math.h>
 
 #define PI 3.141592654
 #define SUN_COLOR 0xf549f3
 #define RAY_COLOR 0xf549f3
 #define WIN_WIDTH 1000
 #define WIN_HEIGHT 500
+typedef struct Object{
+    void *shape;
+    int (*detect)(double x, double y, void *shape);
+    void (*draw)(void *shape, SDL_Window *window, Uint32 color);
+}Object;
 
 typedef struct Circle{
     double x;
@@ -30,66 +34,53 @@ typedef struct RayCluster{
     int numRays;
 }RayCluster;
 
-
 double getDistance(double x1, double y1, double x2, double y2){
     return sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
 }
-int detectObstacle(double x, double y, Room room){
-    if(x >= room.x && x < room.x + room.width &&
-        y >= room.y && y < room.y + room.thickness){
+
+int detectRoom(double x, double y, void* roomObject){
+    Room *room = (Room*)roomObject;
+    if(x >= room->x && x < room->x + room->width &&
+        y >= room->y && y < room->y + room->thickness){
         return 1;
     }
 
-    if(x >= room.x && x < room.x + room.width &&
-        y >= room.y + room.height - room.thickness &&
-        y < room.y + room.height){
+    if(x >= room->x && x < room->x + room->width &&
+        y >= room->y + room->height - room->thickness &&
+        y < room->y + room->height){
         return 1;
     }
 
-    if(x >= room.x && x < room.x + room.thickness &&
-        y >= room.y + room.thickness &&
-        y < room.y + room.thickness + (int)(0.25 * (room.height - 2 * room.thickness))){
+    if(x >= room->x && x < room->x + room->thickness &&
+        y >= room->y + room->thickness &&
+        y < room->y + room->thickness + (int)(0.25 * (room->height - 2 * room->thickness))){
         return 1;
     }
 
-    if(x >= room.x && x < room.x + room.thickness &&
-        y >= room.y + room.thickness + (int)(0.75 * (room.height - 2 * room.thickness)) &&
-        y < room.y + room.height - room.thickness){
+    if(x >= room->x && x < room->x + room->thickness &&
+        y >= room->y + room->thickness + (int)(0.75 * (room->height - 2 * room->thickness)) &&
+        y < room->y + room->height - room->thickness){
         return 1;
     }
 
-    if(x >= room.x + room.width - room.thickness && x < room.x + room.width &&
-        y >= room.y + room.thickness &&
-        y < room.y + room.height - room.thickness){
+    if(x >= room->x + room->width - room->thickness && x < room->x + room->width &&
+        y >= room->y + room->thickness &&
+        y < room->y + room->height - room->thickness){
         return 1;
     }
 
     return 0;
 }
 
-void USR_BuildCircle(struct Circle circle, SDL_Window *window, Uint32 color){
-    SDL_Surface* win_surface =  SDL_GetWindowSurface(window);
-
-    for(int x = circle.x - circle.radius ; x <= circle.x + circle.radius ; x++){
-        for(int y = circle.y - circle.radius ; y <= circle.y + circle.radius ; y++){
-            if( getDistance(x, y, circle.x, circle.y) <= circle.radius){
-                SDL_Rect rect = (SDL_Rect) {x, y, 1, 1}; // x, y, w, h
-                SDL_FillRect(win_surface, &rect, color);
-            } 
-        }
-    }
-    
-    SDL_UpdateWindowSurface(window);
-}
-
-void USR_BuildRoom(Room room, SDL_Window *window){
+void drawRoom(void *roomObject, SDL_Window *window, Uint32 color){
+    Room *room = (Room*)roomObject;
     SDL_Surface* win_surface =  SDL_GetWindowSurface(window);
     
-    int x = room.x;
-    int y = room.y;
-    int height = room.height;
-    int width = room.width;
-    int thickness = room.thickness;
+    int x = room->x;
+    int y = room->y;
+    int height = room->height;
+    int width = room->width;
+    int thickness = room->thickness;
 
     SDL_Rect top = (SDL_Rect) {x, y, width, thickness};
     SDL_FillRect(win_surface, &top, 0x0de03b);
@@ -105,6 +96,24 @@ void USR_BuildRoom(Room room, SDL_Window *window){
     SDL_UpdateWindowSurface(window);
 }
 
+void drawCircle(void *circleObject, SDL_Window *window, Uint32 color){
+    Circle *circle = (Circle*)circleObject;
+    SDL_Surface* win_surface =  SDL_GetWindowSurface(window);
+
+    for(int x = circle->x - circle->radius ; x <= circle->x + circle->radius ; x++){
+        for(int y = circle->y - circle->radius ; y <= circle->y + circle->radius ; y++){
+            if( getDistance(x, y, circle->x, circle->y) <= circle->radius){
+                SDL_Rect rect = (SDL_Rect) {x, y, 1, 1}; // x, y, w, h
+                SDL_FillRect(win_surface, &rect, color);
+            } 
+        }
+    }
+    
+    SDL_UpdateWindowSurface(window);
+}
+
+int detectCircle(double x, double y, void* roomObject){}
+
 void USR_RenderRays(RayCluster cluster, SDL_Window *window, Uint32 color, Room room){
     double xOrigin = cluster.x;
     double yOrigin = cluster.y;
@@ -119,7 +128,7 @@ void USR_RenderRays(RayCluster cluster, SDL_Window *window, Uint32 color, Room r
             double x = xOrigin + d * cos(PI * angle / 180.0);
             double y = yOrigin + d * sin(PI * angle / 180.0);
 
-            if(x < 0 || y < 0 || x > 1000 || y > 500 || detectObstacle(x, y, room)){
+            if(x < 0 || y < 0 || x > 1000 || y > 500 || detectRoom(x, y, &room)){
                 break;
             }else{
                 SDL_Rect right = (SDL_Rect) {x, y, 1, 1};
@@ -141,9 +150,14 @@ int main(){
     SDL_FillRect(win_surface, &rect, 0x0de03b);
     SDL_UpdateWindowSurface(window);
     
-    Circle sun = (Circle) {200, 200, 50};
     RayCluster sunRays = (RayCluster) {200, 200, 150};
+    
+    Object objects[10];
+    Circle sun = {200, 200, 50};
     Room room = {750, 250, 100, 200, 10};
+
+    Object room1Object = {&room, &detectRoom, &drawRoom};
+    Object circle1Object = {&sun, &detectCircle, &drawCircle};
 
     SDL_Event event;
 
@@ -165,9 +179,9 @@ int main(){
                     } 
             }
             
-            USR_BuildCircle(sun, window, SUN_COLOR);
+            drawCircle(&sun, window, SUN_COLOR);
             USR_RenderRays(sunRays, window, RAY_COLOR, room);
-            USR_BuildRoom(room, window);
+            drawRoom(&room, window, SUN_COLOR);
             // clear canvas before each frame
             SDL_FillRect(win_surface, &win_rect, 0x000000);
         }
@@ -176,4 +190,5 @@ int main(){
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
+
 }
